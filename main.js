29fd79ae74f2f2b776649f9a72865e1740606fd6
DevNico2905 <nicobernal2905@gmail.com>
2026-02-11 21:48:40 -0500

    Ready to compile, I hope so..

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Cargar el index.html del build de Angular
+  win.loadFile(path.join(__dirname, 'dist/bolera/browser/index.html'));
+  
+  // Abrir DevTools (opcional, para debug)
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
