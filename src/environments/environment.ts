9d2b79887866cf10f67b21ef532ebf5d1daf2723
DevNico2905 <nicobernal2905@gmail.com>
2026-03-31 00:25:30 -0500

    feat: add environment configurations, update gitignore, and increase bowling timer duration

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://hmmlzvmjkhyiunlqwlwv.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtbWx6dm1qa2h5aXVubHF3bHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDE1NTMsImV4cCI6MjA5MDMxNzU1M30._Z76v5U-mOSahtc4NqujfnJcTnirltu0pfqaLiLj-lg',
+};
