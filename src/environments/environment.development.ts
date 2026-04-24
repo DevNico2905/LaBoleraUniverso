1b25b29d52a1f420fb0447046c1fb21ae73abbf8
DevNico2905 <nbernal36@unisalle.edu.co>
2026-04-24 12:43:16 -0500

    feat: redesign bowling-scorer UI and remove environment file from tracking

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://hmmlzvmjkhyiunlqwlwv.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtbWx6dm1qa2h5aXVubHF3bHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDE1NTMsImV4cCI6MjA5MDMxNzU1M30._Z76v5U-mOSahtc4NqujfnJcTnirltu0pfqaLiLj-lg',
+    // IMPORTANT: Generate a random secret and set the same value in Vercel as API_SECRET
+    apiSecret: '5762c14663678f9f7c9cd6298deb5a0bde05777833dcf4ae133e440ab222e16c',
+};
